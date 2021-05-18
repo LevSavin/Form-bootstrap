b6c30156300fcc421ce9e5b17e1c05e6e31dfb5c
LevSavin <skif-sw@mail.ru>
2021-05-18 21:20:37 +0300

    add minimize css

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const miniCss = require('mini-css-extract-plugin');
 
 module.exports = {
-   mode: 'development',
+   mode: 'production',
    entry: './src/index.js',
    output: {
       path: path.resolve(__dirname, 'dist'),
@@ -19,10 +19,11 @@ module.exports = {
             }
          },
          {
-            test: /\.css$/,
+            test: /\.(s*)css$/,
             use: [
                miniCss.loader,
-               'css-loader'
+               'css-loader',
+               'sass-loader',
             ]
          }
       ]
